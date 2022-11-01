0ab7ca36b9985a8a6fbe4b86a248f8a29a067276
nourbalaha <nourbalaha0@gmail.com>
2022-11-02 02:22:14 +0900

    feat: implement file downloader

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export const FileDownloader = (filename: string): string => {
-  return "implement me";
-};
+function download(data: any, filename: string, mime: string, bom: Uint8Array) {
+  var blobParts: BlobPart[] = typeof bom !== 'undefined' ? [bom, data] : [data];
+  var blobOptions: BlobPropertyBag = { type: mime || "" }
+  var blob: Blob = new Blob(blobParts, blobOptions);
+  var url: string = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(blob) : window.webkitURL.createObjectURL(blob);
+
+  var anchor: HTMLAnchorElement = document.createElement('a');
+  anchor.style.display = 'none';
+  anchor.href = url;
+  anchor.setAttribute('download', filename);
+
+  if (typeof anchor.download === 'undefined') {
+    anchor.setAttribute('target', '_blank');
+  }
+
+  document.body.appendChild(anchor);
+  anchor.click();
+
+  setTimeout(function () {
+    document.body.removeChild(anchor);
+    window.URL.revokeObjectURL(url);
+  }, 200)
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = download;
+}
