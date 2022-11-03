f234bf7f121fb104504bdbb730331d0076db9d9d
Nour Balaha <nourbalaha0@gmail.com>
2022-11-04 02:39:40 +0900

    fix: removed type and fixed test (#2)

--- src/__tests__/file-downloader-js.test.ts
+++ src/__tests__/file-downloader-js.test.ts
@@ -3,12 +3,12 @@ import fileDownloader from "../index";
 test("mock test", () => {
   window.URL.createObjectURL = jest.fn();
   const records = [
-    ["ID", "Name", "Price"],
-    [1, "Orange", 300],
-    [2, "Apple", 2000],
+    ["ID", "Name", "Age"],
+    [1, "Nour", 34],
+    [2, "Mila", 1],
   ];
   const data = records.map((record) => record.join(",")).join("\r\n");
-  const filename = "data.ccsv";
+  const filename = "data.csv";
   const mime = "text/csv";
   const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
 
